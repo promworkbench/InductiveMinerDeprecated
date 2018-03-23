ef788524529c1c402ad2ad4a0616120624d39da0
sleemans <s.leemans@bpm.rwth-aachen.de>
2018-03-23 01:31:39 +0000

    towards efficienttree interface
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveMinerDeprecated/Trunk@38436 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/InductiveMiner/efficienttree/EfficientTreeInt.java
+++ src/org/processmining/plugins/InductiveMiner/efficienttree/EfficientTreeInt.java
@@ -1,5 +1,13 @@
 package org.processmining.plugins.InductiveMiner.efficienttree;
 
 public interface EfficientTreeInt {
+	public static enum NodeType {
+		tau(-1), activity(0), xor(-2), sequence(-3), interleaved(-4), concurrent(-5), or(-6), loop(-7), skip(-8);
 
+		public final int code;
+
+		NodeType(int code) {
+			this.code = code;
+		}
+	}
 }
