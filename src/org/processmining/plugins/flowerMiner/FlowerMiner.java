0d5de502233e5ee18c22d5fbbb3e8beff6d86e80
sleemans <s.leemans@bpm.rwth-aachen.de>
2018-06-13 01:17:07 +0000

    updated Flower Miner to mine EfficientTrees
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveMinerDeprecated/Trunk@39014 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/flowerMiner/FlowerMiner.java
+++ src/org/processmining/plugins/flowerMiner/FlowerMiner.java
@@ -1,5 +1,8 @@
 package org.processmining.plugins.flowerMiner;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.deckfour.xes.classification.XEventClass;
 import org.deckfour.xes.classification.XEventClassifier;
 import org.deckfour.xes.info.XLogInfo;
@@ -18,12 +21,19 @@ import org.processmining.models.graphbased.directed.petrinet.elements.Place;
 import org.processmining.models.graphbased.directed.petrinet.elements.Transition;
 import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetImpl;
 import org.processmining.models.semantics.petrinet.Marking;
+import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTree;
+import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTree.NodeType;
+import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTreeFactory;
+import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTreeImpl;
 import org.processmining.plugins.InductiveMiner.mining.MiningParameters;
 import org.processmining.plugins.InductiveMiner.plugins.dialogs.IMMiningDialog;
 import org.processmining.plugins.connectionfactories.logpetrinet.TransEvClassMapping;
 
-@Plugin(name = "Mine Petri net using Flower Miner", returnLabels = { "Petri net", "Initial marking", "Final marking" }, returnTypes = {
-		Petrinet.class, Marking.class, Marking.class }, parameterLabels = { "Log" }, userAccessible = true, level = PluginLevel.Regular)
+import gnu.trove.map.TObjectIntMap;
+
+@Plugin(name = "Mine Petri net using Flower Miner", returnLabels = { "Petri net", "Initial marking",
+		"Final marking" }, returnTypes = { Petrinet.class, Marking.class,
+				Marking.class }, parameterLabels = { "Log" }, userAccessible = true, level = PluginLevel.Regular)
 public class FlowerMiner {
 
 	@UITopiaVariant(affiliation = IMMiningDialog.affiliation, author = IMMiningDialog.author, email = IMMiningDialog.email)
@@ -87,5 +97,33 @@ public class FlowerMiner {
 		finalMarking.add(sink);
 
 		return new Object[] { net, initialMarking, finalMarking, mapping };
+	}
+
+	public static EfficientTree mine(XLogInfo logInfo) {
+		Collection<XEventClass> classes = logInfo.getEventClasses().getClasses();
+		Iterator<XEventClass> it = classes.iterator();
+
+		//construct activity structures
+		String[] int2activity = new String[classes.size()];
+		TObjectIntMap<String> activity2int = EfficientTreeImpl.getEmptyActivity2int();
+		for (int i = 0; i < classes.size(); i++) {
+			XEventClass id = it.next();
+			int2activity[i] = id.getId();
+			activity2int.put(id.getId(), i);
+		}
+
+		//construct the tree
+		int[] tree = new int[int2activity.length + 4];
+		tree[0] = NodeType.loop.code - 3 * EfficientTreeImpl.childrenFactor;
+		assert (tree[0] < 0);
+		tree[1] = NodeType.xor.code - int2activity.length * EfficientTreeImpl.childrenFactor;
+		assert (tree[1] < 0);
+		for (int i = 0; i < int2activity.length; i++) {
+			tree[2 + i] = i;
+		}
+		tree[2 + int2activity.length] = NodeType.tau.code;
+		tree[3 + int2activity.length] = NodeType.tau.code;
+
+		return EfficientTreeFactory.create(tree, activity2int, int2activity);
 	}
 }
